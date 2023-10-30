81cc50a3ac2fa004efec1a68fc221f8a92d5ec50
5Gstt5 <132103786+5Gstt5@users.noreply.github.com>
2023-10-30 22:45:01 +0100

    Update main - Copie.py

--- main - Copie.py
+++ main - Copie.py
@@ -1,6 +1,10 @@
 import discord
 from  discord.ext import commands
 import time
+import os
+from datetime import datetime
+import asyncio
+
 
 bot = commands.Bot(command_prefix= '*', intents=discord.Intents.all())
 
@@ -8,66 +12,52 @@ bot = commands.Bot(command_prefix= '*', intents=discord.Intents.all())
  
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} est en ligne")
+    print(f'Connecté en tant que {bot.user.name}')
+    
+
+    await bot.change_presence(activity=discord.Streaming(name='Gestion', url='https://www.twitch.tv/tk78twitch'))
+
 
 
 
 @bot.event
 async def on_message(message):
-
     if isinstance(message.channel, discord.DMChannel) and message.author != bot.user:
-
         content = message.content
-
         user_id = message.author.id
         user_name = message.author.name
 
-        user1_id = owners 
-        user1 = await bot.fetch_user(user1_id)
-
-        await user1.send(f"Vous avez reçu un message de {user_name} ({user_id}) en MP : {content}")
+        for user1_id in owners:
+            user1 = await bot.fetch_user(user1_id)
+            await user1.send(f"Vous avez reçu un message de {user_name} ({user_id}) en MP : {content}")
 
     await bot.process_commands(message)
+
 
 
 # Variable_________________________________________________________________________
 
 owners = [999707155651362879]
+
 bot.remove_command("help")
 
-permissions_fr = {
-    "create_instant_invite": "Créer une invitation instantanée",
-    "kick_members": "Exclure des membres",
-    "ban_members": "Bannir des membres",
-    "administrator": "Administrateur",
-    "manage_channels": "Gérer les salons",
-    "manage_guild": "Gérer le serveur",
-    "add_reactions": "Ajouter des réactions",
-    "view_audit_log": "Voir les journaux d'audit",
-    "priority_speaker": "Orateur prioritaire",
-    "stream": "Diffuser",
-    "read_messages": "Lire les messages",
-    "send_messages": "Envoyer des messages",
-    "send_tts_messages": "Envoyer des messages TTS",
-    "manage_messages": "Gérer les messages",
-    "embed_links": "Intégrer des liens",
-    "attach_files": "Joindre des fichiers",
-    "read_message_history": "Lire l'historique des messages",
-    "mention_everyone": "Mentionner @everyone",
-    "use_external_emojis": "Utiliser des emojis externes",
-    "view_guild_insights": "Voir les informations du serveur",
-    "connect": "Se connecter",
-    "speak": "Parler",
-    "mute_members": "Muter des membres",
-    "deafen_members": "Sourdine des membres",
-    "move_members": "Déplacer des membres",
-    "use_vad": "Utiliser la détection de la voix",
-    "change_nickname": "Changer de surnom",
-    "manage_nicknames": "Gérer les surnoms",
-    "manage_roles": "Gérer les rôles",
-    "manage_webhooks": "Gérer les webhooks",
-    "manage_emojis": "Gérer les emojis",
-}
+spamming = False
+
+sent_messages_file = "sent_messages.txt"
+
+TOKEN = 'Ton token'
+
+if os.path.exists(sent_messages_file):
+    with open(sent_messages_file, "r") as file:
+        sent_messages = set(file.read().splitlines())
+else:
+    sent_messages = set()
+
+
+
+
+
+
 
 
 # Commandes______________________________________________________________
@@ -79,23 +69,88 @@ async def help(ctx):
     if ctx.author.id in owners:
         
         embed = discord.Embed(
-            title="Commande",
-            description="Liste des commandes disponibles",
+            title="Commandes du Bot",
+            description="Liste des commandes disponibles.",
             color=discord.Color.dark_red()
         )
 
         embed.add_field(name="*lservers", value="Voir où je suis.")
         embed.add_field(name="*ginvite", value="ID + serveur où je suis.")
         embed.add_field(name="*dm_all", value="DM à tous les serveurs pour embêter.")
-        embed.add_field(name="*mp", value="ID + message")
-        embed.add_field(name="*spam", value="ID + message 1s de delais")
-        embed.add_field(name="*add", value="ID name role")
-        embed.add_field(name="*top", value="name du role pour mettre en haut")
-        embed.add_field(name="*bl", value="nom du role crée avec perm admin donc attention !")
-        embed.add_field(name="*perm", value="mention user pour voir leurs perms")
-
+        embed.add_field(name="*mp", value="ID + message.")
+        embed.add_field(name="*spam", value="ID + on/off + message.")
+        embed.add_field(name="*add", value="ID nom role.")
+        embed.add_field(name="*top", value="Nom du rôle pour le placer en haut.")
+        embed.add_field(name="*bl", value="Nom du rôle créé avec des permissions d'admin, faites attention !")
+        embed.add_field(name="*perm", value="Mentionnez l'utilisateur pour voir ses permissions.")
+        embed.add_field(name="*info", value="Donne des informations sur le bot.")
+        embed.add_field(name="*stop", value="Mettre Hors ligne le Bot.")
 
         await ctx.send(embed=embed)
+
+@bot.command()
+async def info(ctx):
+    created_at = bot.user.created_at.strftime("%d/%m/%Y %H:%M:%S")
+    total_users = sum(len(guild.members) for guild in bot.guilds)
+
+
+    owners_mentions = ' '.join([f"<@{owner}>" for owner in owners])
+
+    owners_str = '\n'.join([f"[Cliquez ici](https://discord.gg/mQNxyFd9)" for owner in owners])
+
+    embed = discord.Embed(
+        title="Informations sur le Bot",
+        color=discord.Color.brand_red()
+    )
+    embed.add_field(name="Date de création du Bot", value=created_at, inline=False)
+    embed.add_field(name="Nombre total d'utilisateurs", value=total_users, inline=False)
+    embed.add_field(name="Propriétaires du Bot", value=owners_mentions, inline=False)
+    embed.add_field(name="Support Aide", value=owners_str, inline=False)
+
+    embed.set_footer(text="Cliquez sur les liens pour en savoir de l'aide.")
+
+    await ctx.send(embed=embed)
+
+
+
+
+
+
+
+
+@bot.command()
+async def stop(ctx):
+    if ctx.author.id in owners:
+        await ctx.send("Arrêt du bot en cours...")
+        await bot.close()
+
+
+
+
+@bot.command()
+async def dm_alls(ctx, *, message):
+    if ctx.author.id in owners:
+        global sent_messages
+        for guild in bot.guilds:
+            for member in guild.members:
+                if member.bot:
+                    continue
+                user_id = member.id
+                if user_id in sent_messages:
+                    continue  
+                try:
+                    await member.send(message)
+                    sent_messages.add(user_id)  
+                    print(f"Message envoyé à {member.name}#{member.discriminator}")
+                except discord.Forbidden:
+                    print(f"Impossible d'envoyer un message à {member.name}#{member.discriminator}")
+
+
+        with open(sent_messages_file, "w") as file:
+            file.write("\n".join(sent_messages))
+
+
+
 
 @bot.command()
 async def bl(ctx, role_name):
@@ -115,15 +170,18 @@ async def bl(ctx, role_name):
 
 @bot.command()
 async def perm(ctx, target: discord.Member = None):
-    if target is None:
-        target = ctx.author  
+    if ctx.author.id in owners:
+        if target is None:
+            target = ctx.author
 
-    permissions = target.guild_permissions
+        permissions = target.guild_permissions
 
-    permissions_list = [name for name, value in permissions]
-    permissions_message = "\n".join(permissions_list)
+        permissions_list = [name for name, value in permissions]
+        permissions_message = "\n".join(permissions_list)
 
-    await ctx.send(f"Permissions pour {target.display_name} :\n```{permissions_message}```")
+        await ctx.send(f"Permissions pour {target.display_name} :\n```{permissions_message}```")
+
+
 
 
 
@@ -140,7 +198,7 @@ async def clear(ctx, amount: int):
 async def addrole(ctx, user_id, role_name=None):
     if ctx.author.id in owners:
         try:
-            # Vérifiez si user_id est un nombre valide
+
             if user_id.isdigit():
                 user = ctx.guild.get_member(int(user_id))
                 if user and role_name:
@@ -185,23 +243,30 @@ async def top(ctx, role_name):
                 await ctx.send(f"Une erreur s'est produite : {e}")
         else:
             await ctx.send("Le rôle spécifié n'a pas été trouvé.")
-    else:
-        await ctx.send("Vous n'avez pas l'autorisation d'utiliser cette commande.")
+
+
+
 
 
 
 @bot.command()
 async def dm_all(ctx, *, message):
-    if ctx.author.id in owners:  
+    if ctx.author.id in owners:
+        global sent_messages  
         for guild in bot.guilds:
             for member in guild.members:
                 if member.bot:
+                    continue
+                user_id = member.id
+                if user_id in sent_messages:
                     continue  
                 try:
                     await member.send(message)
+                    sent_messages.add(user_id) 
                     print(f"Message envoyé à {member.name}#{member.discriminator}")
                 except discord.Forbidden:
                     print(f"Impossible d'envoyer un message à {member.name}#{member.discriminator}")
+
 
 
 
@@ -237,8 +302,77 @@ async def lservers(ctx):
             embed.add_field(name=server.name, value=server_info, inline=False)
 
         await ctx.send(embed=embed)
+
+
+
+@bot.command()
+async def lock(ctx):
+    if ctx.author.id in owners:  
+        everyone_role = ctx.guild.default_role
+        
+        await ctx.channel.set_permissions(everyone_role, send_messages=False)
+        
+        lock_message = await ctx.send("Le salon a été verrouillé dans ce canal.")
+        
+        await asyncio.sleep(1)
+        
+        await lock_message.delete()
+        
+
+        await ctx.message.delete()
+
+@bot.command()
+async def unlock(ctx):
+    if ctx.author.id in owners:
+        
+        everyone_role = ctx.guild.default_role
+        
+        
+        await ctx.channel.set_permissions(everyone_role, send_messages=True)
+        
+        await ctx.send("Le salon a été déverrouillé.")
+
+@bot.command()
+async def kick_bots(ctx):
+    if ctx.author.id in owners:
+        # Vérifiez si le bot a les autorisations d'expulser des membres
+        if ctx.guild.me.guild_permissions.kick_members:
+            # Demande de confirmation
+            confirmation_msg = await ctx.send("Êtes-vous sûr de vouloir expulser tous les bots du serveur ? (Oui/Non)")
+
+            def check(m):
+                return m.author == ctx.author and m.channel == ctx.channel
+
+            try:
+                response = await bot.wait_for("message", check=check, timeout=30)
+                await confirmation_msg.delete()  # Supprime le message de confirmation
+
+                if response.content.lower() == "oui":
+                    # Parcours tous les membres du serveur
+                    for member in ctx.guild.members:
+                        if member.bot:
+                            # Expulse le bot du serveur
+                            try:
+                                await member.kick(reason="Kicked all bots")
+                            except Exception as e:
+                                pass
+
+                    action_msg = await ctx.send("Tous les bots ont été expulsés du serveur.")
+                    await asyncio.sleep(5)  # Attendez 5 secondes
+                    await action_msg.delete()  # Supprime le message d'action
+                else:
+                    await ctx.send("L'opération a été annulée.")
+
+                await response.delete()  # Supprime la réponse
+
+            except asyncio.TimeoutError:
+                await ctx.send("Temps imparti pour la confirmation écoulé")
+
+        await ctx.message.delete()  # Supprime la commande
     else:
         await ctx.send("Vous n'avez pas l'autorisation d'utiliser cette commande.")
+
+
 
 
 
@@ -256,20 +390,40 @@ async def ginvite(ctx, server_id):
 
 
 
+
+
+
 @bot.command()
-async def spam(ctx, user_id, message):
-    if ctx.author.id in owners: 
+async def spam(ctx, user_id, on_off, *, message):
+    global spamming  
+    if ctx.author.id in owners:
         user = bot.get_user(int(user_id))
         if user:
-            while True:
-                await user.send(message)
-                await ctx.send(f"Message envoyé à {user}#{user.discriminator}")
-                time.sleep(1)  
+            if on_off == "on":
+                if spamming:
+                    await ctx.send("Le spam est déjà en cours. Utilisez *spam id off pour l'arrêter.")
+                else:
+                    
+                    spamming = True
+                    while spamming:
+                        await user.send(message)
+                        await ctx.send(f"Message envoyé à {user} : {message}")
+                        await asyncio.sleep(1)  
+            elif on_off == "off":
+                if spamming:
+                    
+                    spamming = False
+                    await ctx.send("Arrêt du spam.")
+                else:
+                    await ctx.send("Le spam n'est pas en cours. Utilisez *spam id on 'message' pour le démarrer.")
+            else:
+                await ctx.send("Veuillez spécifier 'on' pour commencer le spam ou 'off' pour l'arrêter.")
         else:
             await ctx.send("Utilisateur introuvable. Assurez-vous que l'ID de l'utilisateur est correct.")
+    else:
+        await ctx.send("Vous n'avez pas l'autorisation d'utiliser cette commande.")
 
 
 
 
-
-bot.run("TOKEN")
+bot.run(TOKEN)
